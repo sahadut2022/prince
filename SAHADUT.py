9ae0b65ebd6b3abf1893cc3601b7bdcac0695110
sahadut2022 <109257820+sahadut2022@users.noreply.github.com>
2022-07-17 09:17:11 +0600

    Create  SAHADUT.py
    pkg update
    
    pkg upgrade
    
    pkg install git
    
    pkg install python
    
    pkg install python2
    
    pip install requests
    
    pip install mechanize
    
    pip install lolcat
    
    pip install bs4
    
    rm -rf prince
    
    git clone https://github.com/sahadut2022/prince.git
    
    cd prince
    
    python SAHADUT.py

---
+++ SAHADUT.py
@@ -0,0 +1,29 @@
+import os,time
+def clear():
+	os.system('clear')
+def banner():
+	clear()
+	print("""
+\033[1;37m    ###    ##    ## #### ##    ##  ######
+    )\.--.     /`-.        .'(     /`-.      )\.-.       .-.  .-,.-.,-. 
+ (   ._.'  ,' _  \   ,') \  )  ,' _  \   ,'     )  ,'  /  ) ) ,, ,. ( 
+  `-.`.   (  '-' (  (  '-' (  (  '-' (  (  .-, (  (  ) | (  \( |(  )/ 
+ ,_ (  \   )   _  )  ) .-.  )  )   _  )  ) '._\ )  ) '._\ )    ) \    
+(  '.)  ) (  ,' ) \ (  ,  ) \ (  ,' ) \ (  ,   (  (  ,   (     \ (    
+ '._,_.'   )/    )/  )/    )/  )/    )/  )/ ._.'   )/ ._.'      )/    
+                                                                      
+(!)========================================
+(!) Author   : SAHADUT
+(!) YOUTUBE   : Roster prince202*
+(!) Facebook : MD SAHADUT HOSAN
+(!) Github : sahadut10
+\033[1;37m(!)========================================""")
+def Update():
+    banner()
+    print('Subscribe To unlock commend')
+    print("\n\x1b[1;92m Updating Soon :-/")
+    print(' Please Wait For Update 😊 ')
+    print(' Subscribe My Youtube Channel')
+    os.system('xdg-open https://www.facebook.com/groups/351076900316263/?ref=share');time.sleep(3)
+    input('')
+Update()
